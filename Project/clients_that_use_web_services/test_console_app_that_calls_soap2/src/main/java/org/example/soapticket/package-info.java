61be250120b3a03ce78c346a99e80062487a34d4
brownhilludeh <budeh@hollandcollege.com>
2025-11-30 10:12:35 -0400

    Sprint 5 ready for test. Running and finalised

--- Project/clients_that_use_web_services/test_console_app_that_calls_soap2/src/main/java/org/example/soapticket/package-info.java
+++
@@ -1,4 +0,0 @@
-@javax.xml.bind.annotation.XmlSchema(
-        namespace = "http://soap.trainticket.hccis.ca/"
-)
-package org.example.soapticket;
--- Project/train_ticket_tracker/src/main/java/ca/hccis/t3/soap/TrainTicketServicePublisher.java
+++ Project/train_ticket_tracker/src/main/java/ca/hccis/t3/soap/TrainTicketServicePublisher.java
@@ -8,17 +8,17 @@ public class TrainTicketServicePublisher {
                 "http://0.0.0.0:8083/trainticketservice",
                 new TrainTicketServiceImpl()
         );
-//
-//        System.out.println("SOAP TrainTicket service running on:");
-//        System.out.println(" - http://localhost:8083/trainticketservice?wsdl");
-//        System.out.println(" - http://" + getLocalIp() + ":8083/trainticketservice?wsdl");
+
+        System.out.println("SOAP TrainTicket service running on:");
+        System.out.println(" - http://localhost:8083/trainticketservice?wsdl");
+        System.out.println(" - http://" + getLocalIp() + ":8083/trainticketservice?wsdl");
     }
 
-//    private static String getLocalIp() {
-//        try {
-//            return java.net.InetAddress.getLocalHost().getHostAddress();
-//        } catch (Exception ex) {
-//            return "your-local-ip";
-//        }
-//    }
+    private static String getLocalIp() {
+        try {
+            return java.net.InetAddress.getLocalHost().getHostAddress();
+        } catch (Exception ex) {
+            return "your-local-ip";
+        }
+    }
 }
